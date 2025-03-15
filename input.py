05b78a5ca218b40c53d02a028266c2be2bd01aec
regina-SIM <d.regina.9128@gmail.com>
2025-03-15 22:35:22 +0900

    1번째 커밋

---
+++ input.py
@@ -0,0 +1,10 @@
+# 사용자가 입력하 값은 문자열 입니다.
+variable = input()
+print("input String %s, Ineger %d" % (variable, int(variable)))
+
+variable = input(10)
+print(variable)
+
+      
+
+# 사용자에게 메시지를 표현하면서 입력받을 수 있습니다.
